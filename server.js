dc9856b0cc7b1ca6f56958c8dd4266d4eb5c1af6
twiertzema <travis.wiertzema@gmail.com>
2017-11-20 22:00:02 -0600

    added 'hello world' setup

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+app.get("/", (req, res, next) => {
+    let indexFile = path.join(__dirname + "/index.html");
+    res.sendFile(indexFile, err => {
+        if (err) {
+            next(err);
+        } else {
+            console.log(`Sent: ${indexFile}`);
+        }
+    });
+});
+
+app.get("/assets/bundle.js", (req, res) => {
+    let bundleFile = path.join(__dirname + "/build/bundle.js");
+    res.sendFile(bundleFile, err => {
+        if (err) {
+            next(err)
+        } else {
+            console.log(`Sent: ${bundleFile}`);
+        }
+    });
+});
+
+// TODO: Add favicon.ico
+/**
+app.get("/favicon.ico", (req, res) => {
+    let icoFile = path.join(__dirname + "/src/i/favicon.ico");
+    res.sendFile(icoFile, err => {
+        if (err) {
+            next(err)
+        } else {
+            console.log(`Sent: ${icoFile}`);
+        }
+    });
+});
+*/
+
+app.listen(3000, () => {
+    console.log("ReX server now listening on port 3000");
+});
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require("path");
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        path: path.resolve(__dirname, "build"),
+        publicPath: "assets",
+        filename: "bundle.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js/,
+                exclude: /node_modules/,
+                use: "babel-loader"
+            },
+            {
+                test: /\.css/,
+                use: [
+                    "style-loader",
+                    {
+                        loader: "css-loader",
+                        options: {
+                            modules: true
+                        }
+                    },
+                    "postcss-loader"
+                ]
+            },
+            {
+                test: /\.(png|jpg|bmp)/,
+                use: [
+                    "file-loader"
+                ]
+            }
+        ]
+    }
+};
